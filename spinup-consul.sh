d0e363c6c05fb394f69f9e5c6908ce2a94c35db9
lstrand <leonstrand@gmail.com>
2016-09-22 07:52:05 -0700

    added name variable added docker ps at the end

--- spinup-consul.sh
+++ spinup-consul.sh
@@ -3,14 +3,22 @@
 # leonstrand@gmail.com
 
 
+name='consul-1'
+echo
+
 interface='ens33'
 docker run \
   -d \
   --net=host \
-  --name consul01 \
+  --name $name \
   -e CONSUL_BIND_INTERFACE=$interface \
   -e CONSUL_CLIENT_INTERFACE=$interface \
   consul \
     agent \
     -server \
     -bootstrap
+
+echo
+docker ps -f name=$name
+echo
+echo
