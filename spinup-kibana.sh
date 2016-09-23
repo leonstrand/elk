2c14eed88bdb241e3b5b8135dbaf0b2703715981
lstrand <leonstrand@gmail.com>
2016-09-23 11:02:21 -0700

    added dedicated elasticsearch load balancer node for kibana

--- spinup-kibana.sh
+++ spinup-kibana.sh
@@ -13,21 +13,181 @@ if [ -z "$last_container" ]; then
 else
   next_container=${name}-$(expr $(echo $last_container | awk 'BEGIN { FS = "-" } ; { print $NF }') + 1)
 fi
+next_container_elasticsearch_loadbalancer=$next_container'-elasticsearch-loadbalancer'
 echo next_container: $next_container
-
+echo next_container_elasticsearch_loadbalancer: $next_container_elasticsearch_loadbalancer
 
 # identify ip address
 ip=$(ip -o address | awk '$2 !~ /lo|docker/ && $3 ~ /inet$/ {print $4}' | cut -d/ -f1)
 echo ip: $ip
 
-# identify elasticsearch host
-elasticsearch_host=$(curl -sS $ip:8500/v1/health/service/elasticsearch-http?passing | jq -r '.[0] | .Service | .Address + ":" + "\(.Port)"')
-if [ -z "$elasticsearch_host" ]; then
-  echo $0: fatal: no elasticsearch host found
-  exit 1
+# select next free http port over $base_port
+base_port=10000
+last_http_port=$(for i in $(docker ps -qf name=$name); do docker port $i | awk '/^9200/ {print $NF}' | cut -d: -f2; done | sort -n | tail -1)
+if [ -z "$last_http_port" ]; then
+  next_http_port=$(expr $base_port + 9200 + 1)
+else
+  next_http_port=$(expr $last_http_port + 1)
 fi
-elasticsearch_port=$(echo $elasticsearch_host | cut -d: -f2)
-elasticsearch_host=$(echo $elasticsearch_host | cut -d: -f1)
+echo next_http_port: $next_http_port
+# TODO open port check
+
+# select next free transport port over $base_port
+last_transport_port=$(for i in $(docker ps -qf name=$name); do docker port $i | awk '/^9300/ {print $NF}' | cut -d: -f2; done | sort -n | tail -1)
+if [ -z "$last_transport_port" ]; then
+  next_transport_port=$(expr $base_port + 9300 + 1)
+else
+  next_transport_port=$(expr $last_transport_port + 1)
+fi
+echo next_transport_port: $next_transport_port
+# TODO open port check
+
+# discover responsive elasticsearch nodes
+unicast_hosts=$(curl -sS $ip:8500/v1/health/service/elasticsearch-transport?passing | jq -jr '.[] | .Service | .Address + ":" + "\(.Port)" + ","' | sed 's/,$//')
+echo unicast_hosts: $unicast_hosts
+if [ -z "$unicast_hosts" ]; then
+  unicast_hosts=$ip':'$next_transport_port
+fi
+
+#-Des.logger.level=DEBUG
+command="
+docker run -d \
+  --name $next_container_elasticsearch_loadbalancer \
+  -p $next_http_port:9200 \
+  -p $next_transport_port:9300 \
+  elasticsearch \
+  -Dnetwork.host=0.0.0.0 \
+  -Des.node.name=$(hostname)-$next_container_elasticsearch_loadbalancer \
+  -Des.node.master=false \
+  -Des.node.data=false \
+  -Des.cluster.name=elasticsearch-pai \
+  -Dnetwork.publish_host=$ip \
+  -Dhttp.publish_port=$next_http_port \
+  -Dtransport.publish_port=$next_transport_port \
+  -Des.discovery.zen.ping.multicast.enabled=false \
+  -Des.discovery.zen.ping.unicast.hosts=$unicast_hosts \
+  -Dindex.codec=best_compression
+"
+echo command: $command
+result=$(eval $command)
+echo result: $result
+
+
+# consul registration
+echo
+echo
+until curl $ip:$next_http_port 1>/dev/null 2>&1; do
+  #echo -n .
+  echo
+  echo
+  docker ps -f name=$name
+  echo
+  netstat -lnt | egrep 'Active|Proto|$next_http_port'
+  #sleep 0.2
+  sleep 1
+done
+
+echo
+if curl $ip:$next_http_port 1>/dev/null 2>&1; then
+  CONSUL_IP=$ip
+  CONSUL_PORT=8500
+
+  #curl -f --retry 7 --retry-delay 3 \
+  #http://$CONSUL_IP:$CONSUL_PORT/v1/agent/service/register \
+  #-d "$(printf '{"ID":"%s","Name":"elasticsearch","Address":"%s","Port":%s, "Check":{"HTTP": "http://%s:%s", "Interval": "10s"}}' $next_container $PRIVATE_IP $PRIVATE_PORT $PRIVATE_IP $PRIVATE_PORT)"
+
+  # register es transport
+  echo curl -f --retry 7 --retry-delay 3 \
+  http://$CONSUL_IP:$CONSUL_PORT/v1/agent/service/register \
+  -d "$(printf '{
+    "ID":"%s",
+    "Name":"elasticsearch-transport",
+    "Address":"%s",
+    "Port":%s,
+    "Check":{
+      "HTTP": "http://%s:%s",
+      "Interval": "10s"
+    }
+  }' \
+  elasticsearch-transport-$ip:$next_transport_port \
+  $ip \
+  $next_transport_port \
+  $ip \
+  $next_http_port)
+  "
+
+  curl -f --retry 7 --retry-delay 3 \
+  http://$CONSUL_IP:$CONSUL_PORT/v1/agent/service/register \
+  -d "$(printf '{
+    "ID":"%s",
+    "Name":"elasticsearch-transport",
+    "Address":"%s",
+    "Port":%s,
+    "Check":{
+      "HTTP": "http://%s:%s",
+      "Interval": "10s"
+    }
+  }' \
+  elasticsearch-transport-$ip:$next_transport_port \
+  $ip \
+  $next_transport_port \
+  $ip \
+  $next_http_port)
+  "
+
+  # register es http
+  echo curl -f --retry 7 --retry-delay 3 \
+  http://$CONSUL_IP:$CONSUL_PORT/v1/agent/service/register \
+  -d "$(printf '{
+    "ID":"%s",
+    "Name":"elasticsearch-http",
+    "Address":"%s",
+    "Port":%s,
+    "Check":{
+      "HTTP": "http://%s:%s",
+      "Interval": "10s"
+    }
+  }' \
+  elasticsearch-http-$ip:$next_http_port \
+  $ip \
+  $next_http_port \
+  $ip \
+  $next_http_port)
+  "
+
+  curl -f --retry 7 --retry-delay 3 \
+  http://$CONSUL_IP:$CONSUL_PORT/v1/agent/service/register \
+  -d "$(printf '{
+    "ID":"%s",
+    "Name":"elasticsearch-http",
+    "Address":"%s",
+    "Port":%s,
+    "Check":{
+      "HTTP": "http://%s:%s",
+      "Interval": "10s"
+    }
+  }' \
+  elasticsearch-http-$ip:$next_http_port \
+  $ip \
+  $next_http_port \
+  $ip \
+  $next_http_port)
+  "
+fi
+
+echo
+echo $0: elasticsearch http nodes:
+until curl -sS $ip:8500/v1/health/service/elasticsearch-http?passing | jq -jr '.[] | .Service | .Address + ":" + "\(.Port)" + "\n"' | grep -q $next_http_port; do
+  sleep 0.1
+done
+curl -sS $ip:8500/v1/health/service/elasticsearch-http?passing | jq -jr '.[] | .Service | .Address + ":" + "\(.Port)" + "\n"'
+
+echo
+# identify elasticsearch host
+#elasticsearch_host=$(curl -sS $ip:8500/v1/health/service/elasticsearch-http?passing | jq -r '.[0] | .Service | .Address + ":" + "\(.Port)"')
+elasticsearch_host=$ip
+#elasticsearch_port=$(docker inspect kb-1-elasticsearch-loadbalancer | grep http.publish_port | head -1 | cut -d= -f2 | tr -d '",')
+elasticsearch_port=$next_http_port
 echo elasticsearch host: $elasticsearch_host:$elasticsearch_port
 
 # configure kibana build
