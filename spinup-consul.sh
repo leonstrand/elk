1f9370a61549f694e553bb12c0ce0e97da0b6067
lstrand <leonstrand@gmail.com>
2016-09-27 13:46:26 -0700

    simplified docker run command

--- spinup-consul.sh
+++ spinup-consul.sh
@@ -9,15 +9,15 @@ echo
 interface=$(ip -o link | awk '{print $2}' | egrep -v 'lo|loopback|docker' | tr -d :)
 echo $0: interface: $interface
 
-command="docker run \
-  -d \
-  --net=host \
-  --name $name \
-  -e CONSUL_BIND_INTERFACE=$interface \
-  -e CONSUL_CLIENT_INTERFACE=$interface \
-  consul \
-    agent \
-    -server \
+command="docker run
+  -d
+  --net=host
+  --name $name
+  -e CONSUL_BIND_INTERFACE=$interface
+  -e CONSUL_CLIENT_INTERFACE=$interface
+  consul
+    agent
+    -server
     -bootstrap
 "
 echo $command
