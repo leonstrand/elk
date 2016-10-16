690efae354d5b10bbf298c743677f6c30d45cabe
lstrand <leon.strand@medeanalytics.com>
2016-10-16 14:19:06 -0700

    improved output with printf

--- dashboard-container-memory.sh
+++ dashboard-container-memory.sh
@@ -11,14 +11,14 @@ kibana
 '
 
 consul() {
-  docker exec consul-1 ps -o comm,rss | grep consul
+  printf '%s\t\t%8s\n' $(docker exec consul-1 ps -o comm,rss | grep consul)
 }
 logstash() {
   echo
   for container_id in $(docker ps -qf name=logstash); do
     name=$(docker ps -f id=$container_id --format="{{.Names}}")
     memory=$(docker exec $container_id ps -u logstash -o rss=)
-    echo $name $memory
+    printf '%s\t%8d\n' $name $memory
   done | sort -V
 }
 elasticsearch() {
@@ -26,16 +26,17 @@ elasticsearch() {
   for container_id in $(docker ps -qf label=elasticsearch); do
     name=$(docker ps -f id=$container_id --format="{{.Names}}")
     memory=$(docker exec $container_id ps -F | grep java | grep -v grep | awk '{print $6}')
-    echo $name $memory
+    printf '%s\t%8d\n' $name $memory
   done | sort -V
 }
 kibana() {
   echo
-  echo -n kibana-1\ 
-  docker exec kibana-1 ps -AF | grep kibana | grep -v grep | awk '{print $6}'
-  #echo -n kibana-1-elasticsearch-loadbalancer\ 
-  echo -n kibana-1-es-lb\ 
-  docker exec kibana-1-elasticsearch-loadbalancer ps -AF | grep java | grep -v grep | awk '{print $6}'
+  name='kibana-1'
+  memory=$(docker exec kibana-1 ps -AF | grep kibana | grep -v grep | awk '{print $6}')
+  printf '%s\t%8d\n' $name $memory
+  name='kibana-1-es-lb'
+  memory=$(docker exec kibana-1-elasticsearch-loadbalancer ps -AF | grep java | grep -v grep | awk '{print $6}')
+  printf '%s\t%8d\n' $name $memory
 }
 
 
