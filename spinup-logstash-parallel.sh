0e4cb93b7390a827035ee6ce5251903365076ef5
lstrand <leon.strand@medeanalytics.com>
2016-11-07 07:35:22 -0800

    reduced heap size to 2048 since no observation much above 1g has yet been made

--- spinup-logstash-parallel.sh
+++ spinup-logstash-parallel.sh
@@ -68,7 +68,7 @@ echo $0: info: starting container $next_container
 command="
 docker run -d \
   --name $next_container \
-  -e LS_HEAP_SIZE=4096m \
+  -e LS_HEAP_SIZE=2048m \
   -v /pai-logs:/pai-logs \
   -v $directory/logstash/elasticsearch-template.json:/opt/logstash/vendor/bundle/jruby/1.9/gems/logstash-output-elasticsearch-2.7.1-java/lib/logstash/outputs/elasticsearch/elasticsearch-template.json \
   -v $directory/logstash/containers/$next_container/config:/config \
--- spinup-logstash.sh
+++ spinup-logstash.sh
@@ -91,7 +91,7 @@ for server in $servers; do
     command="
     docker run -d \
       --name $next_container \
-      -e LS_HEAP_SIZE=4096m \
+      -e LS_HEAP_SIZE=2048m \
       -v /pai-logs:/pai-logs \
       -v $directory/logstash/elasticsearch-template.json:/opt/logstash/vendor/bundle/jruby/1.9/gems/logstash-output-elasticsearch-2.7.1-java/lib/logstash/outputs/elasticsearch/elasticsearch-template.json \
       -v $directory/logstash/containers/$next_container/config:/config \
