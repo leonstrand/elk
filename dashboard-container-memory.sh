82929bc279d26f4fb0c494ba586837f08d465e75
lstrand <leon.strand@medeanalytics.com>
2016-12-08 08:06:11 -0800

    reformatted output to support double-digit elasticsearch container names

--- dashboard-container-memory.sh
+++ dashboard-container-memory.sh
@@ -11,14 +11,14 @@ kibana
 '
 
 consul() {
-  printf '%s\t\t%8s\n' $(docker exec consul-1 ps -o comm,rss | grep consul)
+  printf '%-16s %8s\n' $(docker exec consul-1 ps -o comm,rss | grep consul)
 }
 logstash() {
   echo
   for container_id in $(docker ps -qf name=logstash); do
     name=$(docker ps -f id=$container_id --format="{{.Names}}")
     memory=$(docker exec $container_id ps -u logstash -o rss=)
-    printf '%s\t%8d\n' $name $memory
+    printf '%-16s %8d\n' $name $memory
   done | sort -V
 }
 elasticsearch() {
@@ -26,17 +26,17 @@ elasticsearch() {
   for container_id in $(docker ps -qf label=elasticsearch); do
     name=$(docker ps -f id=$container_id --format="{{.Names}}")
     memory=$(docker exec $container_id ps -F | grep java | grep -v grep | awk '{print $6}')
-    printf '%s\t%8d\n' $name $memory
+    printf '%-16s %8d\n' $name $memory
   done | sort -V
 }
 kibana() {
   echo
   name='kibana-1'
   memory=$(docker exec kibana-1 ps -AF | grep kibana | grep -v grep | awk '{print $6}')
-  printf '%s\t%8d\n' $name $memory
+  printf '%-16s %8d\n' $name $memory
   name='kibana-1-es-lb'
   memory=$(docker exec kibana-1-elasticsearch-loadbalancer ps -AF | grep java | grep -v grep | awk '{print $6}')
-  printf '%s\t%8d\n' $name $memory
+  printf '%-16s %8d\n' $name $memory
 }
 
 
