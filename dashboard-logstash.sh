4bdd076314f69e2b6354928d5f84fcbff9480437
lstrand <leon.strand@medeanalytics.com>
2017-01-13 08:18:05 -0800

    abstracted consul service name

--- dashboard-logstash.sh
+++ dashboard-logstash.sh
@@ -4,6 +4,7 @@
 
 
 log_directory=/pai-logs
+consul_service_name='logstash-pai'
 
 
 ip=$(ip -o address | awk '$2 !~ /lo|docker/ && $3 ~ /inet$/ && $4 !~ /^169/ {print $4}' | cut -d/ -f1)
@@ -14,12 +15,12 @@ if [ -z "$ip" ]; then
 fi
 
 
-status="$(curl -sS http://$ip:8500/v1/health/checks/logstash 2>/dev/null)"
+status="$(curl -sS http://$ip:8500/v1/health/checks/$consul_service_name 2>/dev/null)"
 case "$status" in
   'No cluster leader'|'')
     echo $0: fatal: status: $status
     echo $0: fatal: attempted status check with:
-    echo curl -sS http://$ip:8500/v1/health/checks/logstash
+    echo curl -sS http://$ip:8500/v1/health/checks/$consul_service_name
     exit 1
   ;;
   *)
@@ -29,9 +30,9 @@ case "$status" in
     echo -e directory\\t\\tserver\\t\\tstatus
     for log_directory in $log_directories; do
     echo -en $log_directory\\t
-    status=$(curl -sS http://$ip:8500/v1/health/checks/logstash | jq -r '.[] | select(.ServiceID=="'$(basename $log_directory)'") | select(.Status=="passing") | .Node + "\t" + .Status')
+    status=$(curl -sS http://$ip:8500/v1/health/checks/$consul_service_name | jq -r '.[] | select(.ServiceID=="'$(basename $log_directory)'") | select(.Status=="passing") | .Node + "\t" + .Status')
     if [ -n "$status" ]; then
-      curl -sS http://$ip:8500/v1/health/checks/logstash | jq -r '.[] | select(.ServiceID=="'$(basename $log_directory)'") | select(.Status=="passing") | .Node + "\t" + .Status'
+      curl -sS http://$ip:8500/v1/health/checks/$consul_service_name | jq -r '.[] | select(.ServiceID=="'$(basename $log_directory)'") | select(.Status=="passing") | .Node + "\t" + .Status'
     else
       echo -e unknown\\t\\tunknown
     fi
