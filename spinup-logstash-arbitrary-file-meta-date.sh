ea38452df17f5874c1b401831a3ef27007e8014c
lstrand <leon.strand@medeanalytics.com>
2017-02-17 14:49:55 -0800

    added date format loop

--- spinup-logstash-arbitrary-file-meta-date.sh
+++ spinup-logstash-arbitrary-file-meta-date.sh
@@ -12,15 +12,30 @@ if [ -z "$directory_data" ] || ! [ -d "$directory_data" ] || [ -z "$date" ] || [
   echo $0: example: $0 /important/log/directory 20170216
   exit 1
 fi
+dates="
+$date
+$(echo $date | sed 's/\([[:digit:]][[:digit:]][[:digit:]][[:digit:]]\)\([[:digit:]][[:digit:]]\)\([[:digit:]][[:digit:]]\)/\1-\2-\3/')
+$(echo $date | sed 's/\([[:digit:]][[:digit:]][[:digit:]][[:digit:]]\)\([[:digit:]][[:digit:]]\)\([[:digit:]][[:digit:]]\)/\1.\2.\3/')
+"
 
+work() {
 echo
+echo
+__date=$1
+echo $0: date: $__date
 echo $0: matching file list and file count:
-echo find "$directory_data" -type f -name \\*"$date"\\* \| tee \>\(wc -l\)
-find "$directory_data" -type f -name \*"$date"\* | tee >(wc -l)
+echo find "$directory_data" -type f -name \\*"$__date"\\* \| tee \>\(wc -l\)
+find "$directory_data" -type f -name \*"$__date"\* | tee >(wc -l)
 sleep 1
 
 echo
 echo $0: spinning up one logstash container per matching file in parallel
 echo $0: parallelism limited to twice the number of cpu cores at a time
-echo cd $directory_work \&\& find "$directory_data" -type f -name \\*"$date"\\* \| time parallel --jobs 200% time ./spinup-logstash-arbitrary-file.sh {} 2\>\&1 \| tee log/spinup-logstash-arbitrary-file.sh.log."$date"
-#cd $directory_work && find "$directory_data" -type f -name \*"$date"\* | time parallel --jobs 200% time ./spinup-logstash-arbitrary-file.sh {} 2>&1 | tee log/spinup-logstash-arbitrary-file.sh.log."$date"
+echo cd $directory_work \&\& find "$directory_data" -type f -name \\*"$__date"\\* \| time parallel --jobs 200% time ./spinup-logstash-arbitrary-file.sh {} 2\>\&1 \| tee log/spinup-logstash-arbitrary-file.sh.log."$__date"
+#cd $directory_work && find "$directory_data" -type f -name \*"$__date"\* | time parallel --jobs 200% time ./spinup-logstash-arbitrary-file.sh {} 2>&1 | tee log/spinup-logstash-arbitrary-file.sh.log."$__date"
+}
+
+
+for date in $dates; do
+  work $date
+done
