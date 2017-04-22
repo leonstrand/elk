0679048ea428c52d4997de8a42d483cc9c12520d
lstrand <lstrand@medeanalytics.com>
2017-04-22 09:32:52 -0700

    abstracted memory conversion and output

--- dashboard-container-memory.sh
+++ dashboard-container-memory.sh
@@ -10,6 +10,10 @@ logstash
 kibana
 '
 
+output_memory_in_megabytes() {
+  memory=$(echo 'scale=0;('$memory'+500)/1000' | bc)
+  printf '%-16s %8s\n' $name ${memory}m
+}
 consul() {
   printf '%-16s %8s\n' $(docker exec consul-1 ps -o comm,rss | grep consul)
 }
@@ -20,8 +24,7 @@ logstash() {
     for container_id in $container_ids; do
       name=$(docker ps -f id=$container_id --format="{{.Names}}")
       memory=$(docker exec $container_id ps -u logstash -o rss=)
-      memory=$(echo 'scale=0;('$memory'+500)/1000' | bc)
-      printf '%-16s %8s\n' $name ${memory}m
+      output_memory_in_megabytes
     done | sort -V
   fi
 }
@@ -30,20 +33,17 @@ elasticsearch() {
   for container_id in $(docker ps -qf label=elasticsearch); do
     name=$(docker ps -f id=$container_id --format="{{.Names}}")
     memory=$(docker exec $container_id ps -u elasticsearch -o rss --no-headers | tr -cd '[:print:]')
-    memory=$(echo 'scale=0;('$memory'+500)/1000' | bc)
-    printf '%-16s %8s\n' $name ${memory}m
+    output_memory_in_megabytes
   done | sort -V
 }
 kibana() {
   echo
   name='kibana-1'
   memory=$(docker exec kibana-1 ps -AF | grep kibana | grep -v grep | awk '{print $6}')
-  memory=$(echo 'scale=0;('$memory'+500)/1000' | bc)
-  printf '%-16s %8s\n' $name ${memory}m
+  output_memory_in_megabytes
   name='kibana-1-es-lb'
   memory=$(docker exec kibana-1-elasticsearch-loadbalancer ps -AF | grep java | grep -v grep | awk '{print $6}')
-  memory=$(echo 'scale=0;('$memory'+500)/1000' | bc)
-  printf '%-16s %8s\n' $name ${memory}m
+  output_memory_in_megabytes
 }
 
 
